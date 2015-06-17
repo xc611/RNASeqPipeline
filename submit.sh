4e3585866ed6a7dc91d9679601b02226ea8dda7a
felloumi <felloumi@gmail.com>
2015-06-17 16:30:36 -0400

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -7,4 +7,4 @@ cd $PBS_O_WORKDIR
 module load python/3.4.0
 module load R/3.1.0
 
-snakemake --jobname 's.{jobid}.{rulename}' --js jobscript.sh -s /data/CCBR/dev/RNA-Seq-pipeline/ver2/rnaseq_wf_v2.py  -k --stats snakemake.stats -T --rerun-incomplete -j 300 --cluster 'qsub {params.batch}'  
+snakemake --jobname 's.{jobid}.{rulename}' --js jobscript.sh -s path2pipeline/rnaseq_wf_v2.py  -k --stats snakemake.stats -T --rerun-incomplete -j 300 --cluster 'qsub {params.batch}'  
