fed03ba38f5fc28fc80b45ca72254acabccd0e4f
felloumi <felloumi@gmail.com>
2015-06-17 16:31:42 -0400

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -6,7 +6,7 @@ cd $PBS_O_WORKDIR
 module load python/3.4.0
 module load R/3.1.0
 
-snakemake --snakefile /data/CCBR/dev/RNA-Seq-pipeline/ver2/rnaseq_wf_v2.py \
+snakemake --snakefile path2pipeline/rnaseq_wf_v2.py \
 --force -j 300 \
 --directory {workdir} --nocolor --notemp --quiet --nolock {job.output} \
 > /dev/null && touch "{jobfinished}" || touch "{jobfailed}"
